4c010277c5931d130e99a9dd36b3ae6f7d4facf0
Chloe Blackwell <chloeblackwell2@hotmail.com>
2020-09-09 20:35:22 +1000

    Added highscores javascript file and made changes to other files

---
+++ Assets/highscores.js
@@ -0,0 +1,30 @@
+// Variables 
+
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector(".clear");
+var back = document.querySelector(".back");
+
+// Event listener to clear scores 
+
+clear.addEventListener('click', function () {
+    localStorage.clear();
+    location.reload();
+});
+
+var combinedScores = localStorage.getItem("combinedScores");
+combinedScores = JSON.parse(combinedScores);
+
+if (combinedScores !== null) {
+
+    for (var i = 0; i < combinedScores.length; i++) {
+
+        var newLi = document.createElement("li");
+        newLi.textContent = combinedScores[i].initials + " " + combinedScores[i].score;
+        highScore.appendChild(newLi);
+    }
+}
+
+// Event listener to move back to index page 
+back.addEventListener('click', function () {
+    window.location.replace("./index.html");
+});
